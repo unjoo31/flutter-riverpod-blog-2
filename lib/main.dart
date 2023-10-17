0132da09c5715c3a21f5a2c91d834e7390efd538
unjoo31 <unjoo31@gmail.com>
2023-10-17 15:10:37 +0900

    git commit "프로젝트 생성"

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_blog/_core/constants/move.dart';
 import 'package:flutter_blog/_core/constants/theme.dart';
 
+// TODO 1: Widget이 아닌 곳에서 현재 화면의 context에 접근해주는 객체
 GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 void main() {
@@ -14,7 +15,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      navigatorKey: navigatorKey, // context가 없는 곳에서 context를 사용할 수 있는 방법 (몰라도 됨)
+      navigatorKey:
+          navigatorKey, // context가 없는 곳에서 context를 사용할 수 있는 방법 (몰라도 됨)
       debugShowCheckedModeBanner: false,
       initialRoute: Move.loginPage,
       routes: getRouters(),
